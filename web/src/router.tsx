401da9b4218313b48afa00a382f348e17bf4a2f1
David BM <correodelnino@gmail.com>
2025-06-24 23:29:27 +0200

    Add Tanstack start template, connect transport

---
+++ web/src/router.tsx
@@ -0,0 +1,36 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { routeTree } from "./routeTree.gen";
+import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
+import { NotFound } from "./components/NotFound";
+import { createConnectTransport } from "@connectrpc/connect-web";
+
+// NOTE: Most of the integration code found here is experimental and will
+// definitely end up in a more streamlined API in the future. This is just
+// to show what's possible with the current APIs.
+
+export function createRouter() {
+  const finalTransport = createConnectTransport({
+    baseUrl: "http://localhost:8080",
+  });
+
+  const queryClient = new QueryClient();
+
+  return routerWithQueryClient(
+    createTanStackRouter({
+      routeTree,
+      context: { queryClient },
+      defaultPreload: "intent",
+      defaultErrorComponent: DefaultCatchBoundary,
+      defaultNotFoundComponent: () => <NotFound />,
+    }),
+    queryClient
+  );
+}
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
